7125b986e1e7b20de6cf24a357be5c01e70ecb03
Ilin-Dmitry <dm-ilin@mail.ru>
2023-06-19 17:12:27 +0400

    Add cheking of win conditions

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,21 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
-const Cell = ({number, onClick, isChecked}) => {
-  const [isClicked, setIsClicked] = useState(false);
-  const [sign, setSign] = useState('')
+const Cell = ({number, onClick, symbol}) => {
   function clickCell() {
     onClick(number)
-    setIsClicked(true)
   }
 
-
-  // Проверить необходимость эффекта - setSign можно перенести в clickCell
-  useEffect(() => {
-    isClicked && setSign('x')
-    console.log('ischeked',isChecked)
-  }, [isClicked])
   return (
-      <button className="game__button" onClick={clickCell}>{sign}</button>
+      <button className="game__button" onClick={clickCell}>{symbol}</button>
   );
 };
 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,15 +3,15 @@ import Cell from "./Cell";
 
 const Game = () => {
   const [cellsArray, setCellsArray] = useState([
-    {key: 1, isChecked: false},
-    {key: 2, isChecked: false},
-    {key: 3, isChecked: false},
-    {key: 4, isChecked: false},
-    {key: 5, isChecked: false},
-    {key: 6, isChecked: false},
-    {key: 7, isChecked: false},
-    {key: 8, isChecked: false},
-    {key: 9, isChecked: false},
+    {key: 1, isChecked: false, sign: ''},
+    {key: 2, isChecked: false, sign: ''},
+    {key: 3, isChecked: false, sign: ''},
+    {key: 4, isChecked: false, sign: ''},
+    {key: 5, isChecked: false, sign: ''},
+    {key: 6, isChecked: false, sign: ''},
+    {key: 7, isChecked: false, sign: ''},
+    {key: 8, isChecked: false, sign: ''},
+    {key: 9, isChecked: false, sign: ''},
   ])
 
   const [isClicked, setIsClicked] = useState(false)
@@ -20,17 +20,74 @@ const Game = () => {
    setIsClicked(false)
  }, [isClicked])
 
-  function clickCell(num) {
+  function checkCell(num, symbol) {
     const newCellsArray = cellsArray;
-    newCellsArray[num-1].isChecked = true;
+    newCellsArray[num].isChecked = true;
+    newCellsArray[num].sign = symbol;
     setCellsArray(newCellsArray)
     setIsClicked(true)
+  }
+
+  function clickCell(num) {
+    checkCell(num - 1, 'x' + ' ' + num) // убрать последний num
+    checkVictoryConditions()
+    setCountermove()
+  }
+
+  function finishGame() {
+    console.log('game over')
+  }
+
+  function setCountermove() {
+    const freeCellsArray = cellsArray.filter(cell => {
+      return (cell.isChecked === false)
+    })
+    const randomFreeCell = freeCellsArray[Math.floor(Math.random()*freeCellsArray.length)]
+    if (randomFreeCell) {
+      checkCell(randomFreeCell.key - 1, 'o' + ' ' + randomFreeCell.key) // убрать последний randomFreeCell.key
+      checkVictoryConditions()
+    } else {
+      finishGame()
+    }
+  }
+
+  function checkVictoryConditions() {
+    const winKeys = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [3, 5, 7],
+      [1, 5, 9]
+    ]
+    const xCheckedCells = cellsArray.map(cell => {
+      if (cell.isChecked && cell.sign.startsWith('x')) return cell.key
+    }).filter(Boolean)
+    const oCheckedCells = cellsArray.map(cell => {
+      if (cell.isChecked && cell.sign.startsWith('o')) return cell.key
+    }).filter(Boolean)
+
+    const xWins = winKeys.some(key => {
+      return key.every(num => xCheckedCells.includes(num))
+    })
+    const oWins = winKeys.some(key => {
+      return key.every(num => oCheckedCells.includes(num))
+    })
+
+    if (xCheckedCells.length > 2 && xWins) {
+      return console.log('x wins')
+    } else if (oCheckedCells.length > 2 && oWins) {
+      return console.log('o wins')
+    }
+
   }
 
   return (
       <div className="game">
         {cellsArray.map(cell => {
-            return <Cell key={cell.key} number={cell.key} isChecked={cell.isChecked} onClick={clickCell}/>
+            return <Cell key={cell.key} number={cell.key} symbol={cell.sign} onClick={clickCell}/>
           })}
       </div>
   );
