d2042f54908070d4a64494773a2fa8e3b422b28f
Ilin-Dmitry <dm-ilin@mail.ru>
2023-06-24 16:21:16 +0400

    Add colorful signs

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Cell = ({number, onClick, symbol}) => {
+
+  const [color, setColor] = useState('')
+  useEffect(() => {
+    setColor(() => {
+      if (symbol === 'x') return 'game__button_color_green'
+      else if (symbol === 'o') return 'game__button_color_blue'
+      else return ''
+    })
+  }, [symbol])
+
+
   function clickCell() {
     onClick(number)
   }
 
   return (
-      <button className="game__button" onClick={clickCell}>{symbol}</button>
+      <button className={`game__button ${color}`} onClick={clickCell}>{symbol}</button>
   );
 };
 
